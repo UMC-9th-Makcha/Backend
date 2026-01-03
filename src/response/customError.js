64e35518dafb58d9447f65d8cc65290fe8ad9159
suyeon0421 <157108684+suyeon0421@users.noreply.github.com>
2026-01-03 12:46:42 +0900

    init: 초기 프로젝트 세팅

--- src/response/customError.js
+++ src/response/customError.js
@@ -1,10 +1,18 @@
-export class DuplicateUserEmailError extends Error {
-  errorCode = "U001";
-  statusCode = 409;
-
-  constructor(reason, data) {
-    super(reason);
-    this.reason = reason;
-    this.data = data;
+export class CustomError {
+  constructor(errorCode, message, path, result = {}) {
+    this.errorCode = errorCode;
+    this.message = message;
+    this.path = this.path;
+    this.result = result;
   }
 }
+
+
+// AUTH-401-001 Access Token 만료
+// AUTH-401-002 Refresh Token 만료
+// AUTH-403-001 권한 없음
+// MAP-400-001 잘못된 좌표값
+// MAP-404-001 역 정보 없음
+// MAP-404-002 경로 탐색 실패
+// COM-400-001 필수 파라미터 누락
+// COM-500-001 서버 내부 오류
