49a9acaa90e75317afac533d8f4196b2d1559d57
Chea-yunzi <katete051@naver.com>
2026-01-06 01:14:08 +0900

    ✨ feature/#1 : 카카오톡 로그인 api  구현 완료

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
+import cookieParser from 'cookie-parser'; //카카오 로그인
+import authRouter from './routes/auth.js'; //카카오 로그인
 
 const app = express();
-
 app.use(express.json());
+app.use(cookieParser());
+
+app.use('/auth', authRouter);
+
 
 app.get("/", (req, res) => {
   res.send("server on");
