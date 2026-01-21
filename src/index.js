a183c0fde84a6c12ac4a354ec2c9fca44f331b1d
Chea-yunzi <katete051@naver.com>
2026-01-21 15:58:28 +0900

    import 문 수정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import app from './app.js';
-//import dotenv from "dotenv"; //도커에서 불안정.
-dotenv.config({
-  path: "/app/.env"
-});
+import 'dotenv/config'; //dotenv 자동 로그가 되도록 수정함.
+
 console.log("KAKAO KEY:", process.env.KAKAO_REST_API_KEY);
 const PORT = process.env.PORT || 3000;
 
